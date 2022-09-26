e6ac20ac5de9632cb664b085ad663957e07b483b
JCWin <jcm91186@gmail.com>
2022-09-25 20:31:50 -0700

    Passed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringList = string.split('');
+    let revStrLst = [];
+    let revStr = '';
+    for (let i = stringList.length - 1; i >= 0; i--) {
+        revStrLst.push(stringList[i]);
+    }
+    revStr = revStrLst.join('');
+    return revStr;
 };
 
 // Do not edit below this line
