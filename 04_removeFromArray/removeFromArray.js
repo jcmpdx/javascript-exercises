ac97ee130dc14b5073407459bfe44aa158551482
JCWin <jcm91186@gmail.com>
2022-09-26 15:37:12 -0700

    Passed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    let args = Array.from(arguments);
+    args.splice(0, 1);
+    return array.filter(item => args.indexOf(item) < 0)
 };
 
 // Do not edit below this line
