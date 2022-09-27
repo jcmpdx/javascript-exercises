c07fb2d50c71ffd39eb9e3747afb879108d932b0
JCWin <jcm91186@gmail.com>
2022-09-27 13:56:20 -0700

    Passed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
+  if (c == 0) {
+    return 0; 
+  } return parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = (c * (9/5) + 32);
+  if (f == 32) {
+    return 32;
+  } return parseFloat(f.toFixed(1));
 };
 
 // Do not edit below this line
