83922c9f1f256d7a5abcfcd4e2f5da7e69c1d56b
JCWin <jcm91186@gmail.com>
2022-09-26 20:14:33 -0700

    Passed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let num = 0
+    if (typeof(x) != "number" || x < 0 || typeof(y) != "number" || y < 0 ) {
+        return 'ERROR';
+    }
+    if (x < y) {
+        smallNum = x;
+        bigNum = y;
+    } else {
+        smallNum = y;
+        bigNum = x;
+    }
+    for (let i = bigNum; i >= smallNum; i--) {
+        num += i;
+    }
+    return num;
 };
+
+console.log(sumAll(1, '90'));
 
 // Do not edit below this line
 module.exports = sumAll;
